881b574cf6460b7e32eb5cc45715c401c602b0a5
gitslaton <sblickman@gmail.com>
2017-02-19 17:07:30 -0500

    added onClickCreate method

--- app/src/main/java/edu/hanover/basin/EventCreationActivity.java
+++ app/src/main/java/edu/hanover/basin/EventCreationActivity.java
@@ -3,10 +3,12 @@ package edu.hanover.basin;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import com.facebook.Profile;
 import com.google.android.gms.maps.model.LatLng;
 
 import static edu.hanover.basin.EventDetailsActivity.EXTRA_EVENT_ID;
@@ -22,13 +24,16 @@ public class EventCreationActivity extends Activity {
     private EditText description;
     private EditText Time;
     private DatePicker date;
+    private String facebook_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_creation);
 
+        facebook_id = Profile.getCurrentProfile().getId();
         title = (EditText)findViewById(R.id.title);
+        title.setText(facebook_id);
         description = (EditText)findViewById(R.id.description);
         Time = (EditText)findViewById(R.id.time);
         //duration
@@ -38,11 +43,23 @@ public class EventCreationActivity extends Activity {
         lng = (Double)getIntent().getExtras().get(EXTRA_EVENT_LNG);
         location = String.valueOf(lat) + ", " + String.valueOf(lng);
         description.setText(location);
+
+
+
         //if need to convert from string to LatLng
 //        String[] location = ((String)getIntent().getExtras().get(EXTRA_EVENT_LATLNG)).split(",");
 //        double lat = Double.parseDouble(location[0]);
 //        double lng = Double.parseDouble(location[1]);
 //        LatLng latLng = new LatLng(lat, lng);
 
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        String dateString = sdf.format(date1);
+
     }
+
+    public void onClickCreate(View v){
+
+
+    }
+
 }
