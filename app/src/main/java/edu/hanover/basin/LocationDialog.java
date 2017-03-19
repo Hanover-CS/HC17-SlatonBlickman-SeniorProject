9caa1406ca008cb349066e8a67cf18a19d94e924
gitslaton <sblickman@gmail.com>
2017-03-19 09:42:00 -0400

    created dialog to take user to location settings

---
+++ app/src/main/java/edu/hanover/basin/LocationDialog.java
@@ -0,0 +1,37 @@
+package edu.hanover.basin;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+
+/**
+ * Created by Slaton on 3/16/2017.
+ */
+
+public class LocationDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.location_enabled)
+                .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+//                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+//                        startActivity(intent);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
