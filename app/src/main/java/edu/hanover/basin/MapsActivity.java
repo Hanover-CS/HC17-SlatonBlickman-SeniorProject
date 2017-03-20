b235fd6f02c2b09f61144846d1ddc3be73155c7d
gitslaton <sblickman@gmail.com>
2017-03-20 19:36:57 -0400

    Maps now does not double up on icons when activity is resumed; added dialog with a list view to show events and create them

--- app/src/main/java/edu/hanover/basin/MapsActivity.java
+++ app/src/main/java/edu/hanover/basin/MapsActivity.java
@@ -1,13 +1,22 @@
 package edu.hanover.basin;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListPopupWindow;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -30,9 +39,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 
@@ -51,7 +63,7 @@ public class MapsActivity extends AppCompatActivity
     private LatLng mLastLatLng;
     private GoogleApiClient mGoogleApiClient;
     private Map<Marker, EventMarker> allMarkerMap;
-    private Marker mMe;
+    private EventMarker mMe;
     // Declare a variable for the cluster manager.
     private ClusterManager<EventMarker> mClusterManager;
 
@@ -69,6 +81,7 @@ public class MapsActivity extends AppCompatActivity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,7 +123,7 @@ public class MapsActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-
+        allMarkerMap =  new HashMap<>();
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
 
             @Override public void onMapLongClick(LatLng latlng){
@@ -166,6 +179,8 @@ public class MapsActivity extends AppCompatActivity
 
     }
 
+
+
     @Override
     public void onConnected(Bundle connectionHint) {
         try {
@@ -182,10 +197,10 @@ public class MapsActivity extends AppCompatActivity
             //add rest of markers from database
             basinURL url = new basinURL();
             url.getEventURL("");
-            allMarkerMap =  new HashMap<>();
-            request(Request.Method.GET, url.toString());
-
-            updateUI();
+            if(allMarkerMap != null){
+                request(Request.Method.GET, url.toString());
+                updateUI();
+            }
 
         }
 //        else{
@@ -225,6 +240,8 @@ public class MapsActivity extends AppCompatActivity
         // Initialize the manager with the context and the map.
         // (Activity extends context, so we can pass 'this' in the constructor.)
         mClusterManager = new ClusterManager<EventMarker>(this, mMap);
+        EventRenderer eventRenderer = new EventRenderer(this, mMap, mClusterManager);
+        mClusterManager.setRenderer(eventRenderer);
 
         // Point the map's listeners at the listeners implemented by the cluster
         // manager.
@@ -237,10 +254,43 @@ public class MapsActivity extends AppCompatActivity
                 .setOnClusterClickListener(new ClusterManager.OnClusterClickListener<EventMarker>() {
                     @Override
                     public boolean onClusterClick(final Cluster<EventMarker> cluster) {
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                cluster.getPosition(), (float) Math.floor(mMap
-                                        .getCameraPosition().zoom + 1)), 300,
-                                null);
+                        AlertDialog.Builder builderSingle = new AlertDialog.Builder(MapsActivity.this);
+                        ArrayList<EventMarker> eventMarkers;
+                        eventMarkers = ArrayUtil.toArrayList(cluster);
+
+                        builderSingle.setNegativeButton("Back to Map", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int item) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builderSingle.setPositiveButton("New Event", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int item) {
+                                Collection<EventMarker> collection = cluster.getItems();
+                                EventMarker marker = collection.iterator().next();
+                                LatLng position = marker.getPosition();
+                                Intent intent = new Intent(getApplicationContext(), EventCreationActivity.class);
+                                intent.putExtra(EventCreationActivity.EXTRA_EVENT_LAT, position.latitude);
+                                intent.putExtra(EventCreationActivity.EXTRA_EVENT_LNG, position.longitude);
+                                intent.putExtra(EventCreationActivity.EXTRA_UPDATING, false);
+                                startActivity(intent);
+                            }
+                        });
+                        // Create the adapter to convert the array to views
+                        final EventMarkersAdapter adapter = new EventMarkersAdapter(getApplicationContext(), eventMarkers);
+                        builderSingle.setAdapter(adapter,  new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int item) {
+                                EventMarker event = adapter.getItem(item);
+                                Intent intent = new Intent(getApplicationContext(), EventDetailsActivity.class);
+                                intent.putExtra(EventDetailsActivity.EXTRA_EVENT_ID, event.getID());
+                                intent.setFlags(FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                startActivity(intent);
+                            }
+                        });
+                        builderSingle.show();
+
                         return true;
                     }
                 });
@@ -252,6 +302,7 @@ public class MapsActivity extends AppCompatActivity
                 if(!(eventMarker.getID().equals("-1"))){
                     Intent intent = new Intent(getApplicationContext(), EventDetailsActivity.class);
                     intent.putExtra(EventDetailsActivity.EXTRA_EVENT_ID, eventMarker.getID());
+                    intent.setFlags(FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
                     startActivity(intent);
                 }
             }
@@ -262,45 +313,20 @@ public class MapsActivity extends AppCompatActivity
         //test location
         //mLastLatLng = new LatLng(38.713, -85.459 );
         if(mLastLatLng != null) {
+            if(mClusterManager == null){
+                setupCluster();
+            }
 
-            setupCluster();
             LocationServices.FusedLocationApi.removeLocationUpdates(
                     mGoogleApiClient, this);
             Toast.makeText(this, "location :"+ mLastLatLng, Toast.LENGTH_SHORT).show();
             Log.i("LAST LOCATION", mLastLatLng.toString());
-            try{
-                Double lat, lng;
-                LatLng startLocation;
 
-                lat = Double.parseDouble((String)getIntent().getExtras().get(EXTRA_EVENT_LAT));
-                lng = Double.parseDouble((String)getIntent().getExtras().get(EXTRA_EVENT_LNG));
-                startLocation = new LatLng(lat, lng);
 
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 17f));
-                Log.i("LATLNG FOR EVENT", startLocation.toString());
+            if(mMe == null){
+                mMe = new EventMarker(mLastLatLng.latitude, mLastLatLng.longitude, "Me", "", "-1");
+                mClusterManager.addItem(mMe);
             }
-            catch(Exception e){
-                Log.i("SOME LATLNG", e.toString());
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLastLatLng, 17f));
-            }
-            //LatLng testLoc = new LatLng(mLastLocation.getLatitude() + 0.0001, mLastLocation.getLongitude() + 0.0001);
-            //LatLng cameraPos = new LatLng(30.0, -85.0);
-            //Log.i("CAMERA POS", cameraPos.toString());
-            //LatLng sydney = new LatLng(-34, 151);
-            if(mMe != null){
-                mMe.setPosition(mLastLatLng);
-            }
-            else{
-                EventMarker me = new EventMarker(mLastLatLng.latitude, mLastLatLng.longitude, "Me", "", "-1");
-                mClusterManager.addItem(me);
-                //mMe = mMap.addMarker(new MarkerOptions().position(cameraPos).title("Me"));
-
-            }
-            //mMap.moveCamera(CameraUpdateFactory.newLatLng(cameraPos));
-//            Marker testMark = mMap.addMarker(new MarkerOptions()
-//                    .position(new LatLng(30.0, -85.0))
-//                    .title("test title")
-//                    .snippet("6:00"));
 
         }
         else{
@@ -320,6 +346,10 @@ public class MapsActivity extends AppCompatActivity
         LatLng location;
         String title, time_date, id;
         EventMarker eventMarker;
+
+        mMe = new EventMarker(mLastLatLng.latitude, mLastLatLng.longitude, "Me", "", "-1");
+        mClusterManager.addItem(mMe);
+
         try{
            for(int i = 0; i < events.length(); i++){
                event = events.getJSONObject(i);
@@ -329,10 +359,7 @@ public class MapsActivity extends AppCompatActivity
                location = new LatLng(lat, lng);
                title = event.getString("title");
                time_date = event.getString("time_start") + ", " + event.getString("date");
-//                   marker = mMap.addMarker(new MarkerOptions()
-//                           .position(location)
-//                           .title(title)
-//                           .snippet(time_date));
+
                eventMarker = new EventMarker(lat, lng, title, time_date, id);
                //allMarkerMap.put(marker, event);
                mClusterManager.addItem(eventMarker);
@@ -355,8 +382,25 @@ public class MapsActivity extends AppCompatActivity
                         try{
                             Log.i("event response", response.toString());
                             JSONArray events = response.getJSONArray("events");
-                            addMarkers(events);
+                            if(mClusterManager.getMarkerCollection().getMarkers().size() != events.length()){
+                                mClusterManager.clearItems();
+                                addMarkers(events);
+                            }
+                            try{
+                                Double lat, lng;
+                                LatLng startLocation;
 
+                                lat = Double.parseDouble((String)getIntent().getExtras().get(EXTRA_EVENT_LAT));
+                                lng = Double.parseDouble((String)getIntent().getExtras().get(EXTRA_EVENT_LNG));
+                                startLocation = new LatLng(lat, lng);
+
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 17f));
+                                Log.i("LATLNG FOR EVENT", startLocation.toString());
+                            }
+                            catch(Exception e){
+                                Log.i("SOME LATLNG", e.toString());
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLastLatLng, 17f));
+                            }
 
                         }
                         catch(JSONException e){
