5470ba908ab5bc71601c0b6a3328f157b2cfd671
gitslaton <sblickman@gmail.com>
2017-03-25 04:20:24 -0400

    created stub PHPUnit class to test Slim Application

---
+++ basinWeb/unitTests/routeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Class for doing unit tests on basinWeb routes.
+* followed this guide for installation and instruction:
+*  https://medium.com/@Andela/writing-testable-api-apps-in-slim-framework-29905970941b#.g3hj5rjsh
+*/
+class routeTest extends PHPUnit_Framework_TestCase
+{
+    public function testGet() {
+        $this->assertTrue(true);
+    } 
+}
