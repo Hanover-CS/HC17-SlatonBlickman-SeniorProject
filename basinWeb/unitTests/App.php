33ef11459e4dcd0f0bffca1107b21bb40c3c4ac8
gitslaton <sblickman@gmail.com>
2017-03-27 13:50:56 -0400

    Routes not properly reflected in App.php

--- basinWeb/unitTests/App.php
+++ basinWeb/unitTests/App.php
@@ -9,17 +9,17 @@ namespace blickman\unitTestAPI;
  */
 
 //includes object that handles PDO functionality
-require_once './include/dbOperation.php';
+require_once "./include/dbOperation.php";
 
 //includes helper functions that will validate requests
-require_once './include/validateRequest.php';
+require_once "./include/validateRequest.php";
 
 //includes constants to abstract database access
-//require_once '../../../protected/databaseKeys.php'; only used for release version
-require_once 'testDatabaseKeys.php'; //use localhost for testing
+//require_once "../../../protected/databaseKeys.php"; only used for release version
+require_once "testDatabaseKeys.php"; //use localhost for testing
 
 //includes required Slim files
-require './vendor/autoload.php';
+require "./vendor/autoload.php";
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
@@ -41,16 +41,16 @@ class App
          * Configure application settings
         */
 
-        $config['displayErrorDetails'] = true;
-        $config['addContentLengthHeader'] = false;
+        $config["displayErrorDetails"] = true;
+        $config["addContentLengthHeader"] = false;
 
         /*
         * Use constants from protected folder for database information
         */
-        $config['db']['host']   = DB_HOST;
-        $config['db']['user']   = DB_USERNAME;
-        $config['db']['pass']   = DB_PASSWORD;
-        $config['db']['dbname'] = DB_NAME;
+        $config["db"]["host"]   = DB_HOST;
+        $config["db"]["user"]   = DB_USERNAME;
+        $config["db"]["pass"]   = DB_PASSWORD;
+        $config["db"]["dbname"] = DB_NAME;
 
         //Initalize new Slim application with given settings
         $app = new \Slim\App(["settings" => $config]);
@@ -58,10 +58,10 @@ class App
         $container = $app->getContainer(); 
 
         //use the container from the application to set a PDO object with MYSQL
-        $container['db'] = function ($c) {
-            $db = $c['settings']['db'];
-            $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], 
-                $db['user'], $db['pass']);
+        $container["db"] = function ($c) {
+            $db = $c["settings"]["db"];
+            $pdo = new PDO("mysql:host=" . $db["host"] . ";dbname=" . $db["dbname"], 
+                $db["user"], $db["pass"]);
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -70,9 +70,9 @@ class App
         };
 
         //Can be used if logging is desired
-        //logging gives errors on vault, so it's commented out
-        // $container['logger'] = function($c) {
-        //     $logger = new \Monolog\Logger('my_logger');
+        //logging gives errors on vault, so it"s commented out
+        // $container["logger"] = function($c) {
+        //     $logger = new \Monolog\Logger("my_logger");
         //     $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
         //     $logger->pushHandler($file_handler);
         //     return $logger;
@@ -82,19 +82,13 @@ class App
          * ROUTES
         */
 
-        /**
-        * GET: returns the default page. 
-        * TODO: Return list of links to all available routes 
-        */
-        $app->get("[/]", function(Request $request, Response $response, $args) {
-            $response->getBody()->write("Default page for http requests");
-        }); 
+        require "./v1/routes/indexRouting.php";
 
-        require './v1/routes/usersRouting.php';
+        require "./v1/routes/usersRouting.php";
          
-        require './v1/routes/eventsRouting.php';
+        require "./v1/routes/eventsRouting.php";
 
-        require './v1/routes/attendeesRouting.php';
+        require "./v1/routes/attendeesRouting.php";
 
         $this->app = $app;
     }
